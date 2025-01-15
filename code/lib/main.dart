130c7ef830a7772796cc0893f1cf97f841773692
yonpachi <51481568+yonpachi@users.noreply.github.com>
2025-01-15 23:07:45 +0900

    add package : firebase_core, firebase_auth

--- code/lib/main.dart
+++ code/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../route/route.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const ProviderScope(child: MyApp()));
 }
 
