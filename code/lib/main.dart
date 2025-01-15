8736b5df6d4f6c9fb3cc62cfce698f43f201bc4a
falmiria <over6153@gmail.com>
2025-01-16 02:45:04 +0900

    mainとappを分割

--- code/lib/main.dart
+++ code/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../route/route.dart';
+import 'app.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
--- code/test/widget_test.dart
+++ code/test/widget_test.dart
@@ -8,7 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:code/main.dart';
+//import 'package:code/main.dart';
+import 'package:code/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
