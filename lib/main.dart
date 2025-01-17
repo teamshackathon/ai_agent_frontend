7e04ff38839bbc2784cc0b8f7d44313b70c4f03f
CoCo9122 <banaji.yu0517@outlook.jp>
2025-01-18 00:47:28 +0900

    コードの整形

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
--- lib/toast.dart
+++ lib/toast.dart
@@ -3,14 +3,14 @@ import 'package:logger/logger.dart';
 
 /// 警告トースト（およびログ）を出力
 /// Object?にはnullを含めてあらゆる型のデータが入れられる
-void warningToast({required String toast, Object? log}){
+void warningToast({required String toast, Object? log}) {
   Fluttertoast.showToast(msg: toast);
-  if(log != null)Logger().w(log);
+  if (log != null) Logger().w(log);
 }
 
 /// 情報トースト（およびログ）を出力
 /// Object?にはnullを含めてあらゆる型のデータが入れられる
-void infoToast({required String toast, Object? log}){
+void infoToast({required String toast, Object? log}) {
   Fluttertoast.showToast(msg: toast);
-  if(log != null)Logger().i(log);
+  if (log != null) Logger().i(log);
 }
