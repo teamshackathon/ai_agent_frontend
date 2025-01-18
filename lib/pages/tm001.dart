befd22fb4a5b3064e6307bf4e3b9d7290ee2cf69
yonpachi <51481568+yonpachi@users.noreply.github.com>
2025-01-18 20:09:42 +0900

    add : logout_firebase

--- lib/pages/tm001.dart
+++ lib/pages/tm001.dart
@@ -1,9 +1,13 @@
 import 'package:code/data/room/room.dart';
 import 'package:code/data/person/person.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class TeacherMain extends ConsumerWidget {
+import '../firebase/auth/logout/logout_firebase.dart';
+import '../widget/loading_button.dart';
+
+class TeacherMain extends HookConsumerWidget {
   const TeacherMain({super.key});
 
   @override
@@ -11,10 +15,26 @@ class TeacherMain extends ConsumerWidget {
     final status = ref.watch(personStatusProvider);
     final rooms = ref.watch(roomsProvider);
     final displaySize = MediaQuery.of(context).size;
+    final loading = useState<bool>(false);
 
     return Scaffold(
       appBar: AppBar(),
       body: Column(children: [
+        LoadingButton(
+          text: "登録",
+          width: displaySize.width * 0.28,
+          height: displaySize.width * 0.07,
+
+          // 読込中はここをtrue
+          isLoading: loading.value,
+
+          // 押されたときにログアウト処理
+          onPressed: () async {
+            loading.value = true;
+            await logoutFirebase();
+            loading.value = false;
+          },
+        ),
         SizedBox(
           width: displaySize.width * 0.8,
           height: displaySize.width * 0.1,
