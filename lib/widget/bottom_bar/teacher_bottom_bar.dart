ca9092cf9bcf37b39e101addd54f909f0e6831d4
CoCo9122 <banaji.yu0517@outlook.jp>
2025-01-31 18:59:03 +0900

    先生用のbottombarの変更

--- lib/widget/bottom_bar/teacher_bottom_bar.dart
+++ lib/widget/bottom_bar/teacher_bottom_bar.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class TeacherBottomBar extends StatelessWidget {
+import 'bottom_bar_widget.dart';
+
+class TeacherBottomBar extends HookConsumerWidget {
   const TeacherBottomBar({
     super.key,
     required this.navigationShell,
@@ -12,30 +16,53 @@ class TeacherBottomBar extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // animation controllerが入ったリストを作成
+    final controllers = useState<List<AnimationController>>(
+      List.generate(
+        3,
+        (_) => useAnimationController(
+          duration: const Duration(milliseconds: 500),
+        ),
+      ),
+    );
+
+    final selectedIndex = useState<int>(1);
+
+    // 初回描画時にアニメーションを開始
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      for (final controller in controllers.value) {
+        controller.forward();
+      }
+    });
+
     return Scaffold(
       body: navigationShell,
       floatingActionButton: FloatingActionButton(onPressed: () {}),
       bottomNavigationBar: NavigationBar(
         selectedIndex: navigationShell.currentIndex,
-        destinations: const [
-          NavigationDestination(
+        destinations: [
+          RottieNavigationDestination(
             label: 'プロフィール',
-            icon: Icon(Icons.person_outlined),
-            selectedIcon: Icon(Icons.person),
+            iconPath: 'assets/profile.json',
+            animationController: controllers.value[0],
           ),
-          NavigationDestination(
+          RottieNavigationDestination(
             label: 'ホーム',
-            icon: Icon(Icons.home_outlined),
-            selectedIcon: Icon(Icons.home),
+            iconPath: 'assets/home.json',
+            animationController: controllers.value[1],
           ),
-          NavigationDestination(
+          ActivityLottieNavigationDestination(
             label: '通知',
-            icon: Icon(Icons.notifications_outlined),
-            selectedIcon: Icon(Icons.notifications),
+            iconPath: 'assets/activity.json',
+            animationController: controllers.value[2],
           ),
         ],
         onDestinationSelected: (index) {
+          controllers.value[index].reset();
+          selectedIndex.value = index;
+          controllers.value[index].forward();
+
           navigationShell.goBranch(
             index,
             initialLocation: index == navigationShell.currentIndex,
