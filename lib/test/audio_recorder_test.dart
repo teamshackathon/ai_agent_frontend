6938766df3e7e96a3adc87bf2f7e66dacd3b0b1f
yonpachi <51481568+yonpachi@users.noreply.github.com>
2025-02-02 01:04:42 +0900

    一旦

--- lib/test/audio_recorder_test.dart
+++ lib/test/audio_recorder_test.dart
@@ -1,7 +1,14 @@
-// import 'dart:async';
-// import 'package:flutter/material.dart';
-// import 'package:record/record.dart';
-//
-// final record = AudioRecorder();
-//
-// if(await record.has)
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:record/record.dart';
+
+class AudioRecorderTest extends HookConsumerWidget {
+  const AudioRecorderTest({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final record = AudioRecorder();
+
+    return Placeholder();
+  }
+}
