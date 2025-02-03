aac152b4734241e40ea8c0d0927f085c61c6244e
yonpachi <51481568+yonpachi@users.noreply.github.com>
2025-02-04 02:00:37 +0900

    なんかエラー出てたから直した

--- lib/pages/mq001.dart
+++ lib/pages/mq001.dart
@@ -4,6 +4,9 @@ import 'package:code/dummy/widget/dummy_base_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:http/http.dart' as http;
+
+import '../data/quiz/quiz.dart';
 
 class DummyMain extends HookConsumerWidget {
   const DummyMain({super.key});
