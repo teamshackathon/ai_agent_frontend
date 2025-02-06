be663b30e3b3d49eca0e5f96d04410361083e72f
yonpachi <51481568+yonpachi@users.noreply.github.com>
2025-02-06 17:07:57 +0900

    add : submission, result

---
+++ lib/data/submission/submission.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:code/toast.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'lesson_data.freezed.dart';
+
+@freezed
+class LessonData with _$LessonData{
+  const LessonData._();
+
+  const factory 
+}
