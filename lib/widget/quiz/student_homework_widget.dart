a11d81bdbd8c0c1f30a1b7b207edf6494042cc04
falmiria <over6153@gmail.com>
2025-02-08 00:24:27 +0900

    format

--- lib/widget/quiz/student_homework_widget.dart
+++ lib/widget/quiz/student_homework_widget.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class StudentHomeworkWidget extends ConsumerWidget{
+class StudentHomeworkWidget extends ConsumerWidget {
   const StudentHomeworkWidget({
     super.key,
   });
@@ -12,13 +12,11 @@ class StudentHomeworkWidget extends ConsumerWidget{
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final quizList = ref.read(quizNotifierProvider);
-      return Expanded(
-        
-        child:ListView.builder(
-          itemBuilder:(context,index)=> QuizEditWidget(editable: true, quiz: quizList[index], onChanged: onChanged),
-          itemCount: quizList.length,)
-        
-      );
-    }
+    return Expanded(
+        child: ListView.builder(
+      itemBuilder: (context, index) => QuizEditWidget(
+          editable: true, quiz: quizList[index], onChanged: onChanged),
+      itemCount: quizList.length,
+    ));
   }
 }
