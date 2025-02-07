26708056a1f66b21a0db46ff7dc769ab9ee6d504
falmiria <over6153@gmail.com>
2025-02-08 00:18:09 +0900

    init配置用仮コミット

--- lib/pages/student/main/tools/student_homework.dart
+++ lib/pages/student/main/tools/student_homework.dart
@@ -1,3 +1,7 @@
+import 'package:code/data/firebase/submission_stream.dart';
+import 'package:code/data/quiz/quiz.dart';
+import 'package:code/data/result/result.dart';
+import 'package:code/widget/quiz/student_homework_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -10,6 +14,35 @@ class StudentHomework extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Center(child: Text("宿題"));
+    final homework = ref.watch(submissionStreamProvider);
+    final homeworkNot = QuizNotifier();
+    return homework.when(
+        data: (snap) {
+          if (snap.docs.isEmpty) {
+            return anasked();
+          } else {
+            homeworkNot.init(snap.docs[0].data().homeworks);
+            if (snap.docs[0].data().homeworkResults.isEmpty) {
+              return StudentHomeworkWidget();
+            } else {
+              homeworkNot.readResult(snap.docs[0].data().homeworkResults);
+              return StudentHomeworkWidget();
+            }
+          }
+        },
+        error: (error, stactrace) => errorDisplay(),
+        loading: loading);
+  }
+
+  Widget anasked() {
+    return Center(child: Text("宿題はまだありません。"));
+  }
+
+  Widget errorDisplay() {
+    return Center(child: Text("読み込みに失敗しました。"));
+  }
+
+  Widget loading() {
+    return Icon(Icons.self_improvement);
   }
 }
---
+++ lib/widget/quiz/student_homework_widget.dart
@@ -0,0 +1,24 @@
+import 'package:code/data/quiz/quiz.dart';
+import 'package:code/widget/quiz/quiz_edit_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class StudentHomeworkWidget extends ConsumerWidget{
+  const StudentHomeworkWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final quizList = ref.read(quizNotifierProvider);
+      return Expanded(
+        
+        child:ListView.builder(
+          itemBuilder:(context,index)=> QuizEditWidget(editable: true, quiz: quizList[index], onChanged: onChanged),
+          itemCount: quizList.length,
+        
+      );
+    }
+  }
+}
